e5d9f370932fd3c180cd3dd9fabc4734bcdbc09b
purvik6062 <purvikpanchal6336@gmail.com>
2023-10-22 11:01:20 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Cards from "./components/Cards";
+import PlaygroundDashboard from "./pages/PlaygroundDashboard";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Cards />
+      <PlaygroundDashboard />
     </>
   );
 }
---
+++ src/pages/PlaygroundDashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Cards from "../components/Cards";
+
+function PlaygroundDashboard() {
+  return (
+    <>
+      <Navbar />
+      <Cards />
+    </>
+  );
+}
+
+export default PlaygroundDashboard;
